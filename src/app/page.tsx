b54190a8278bc01f47d2564ac76c1b7cfa4a3ad7
Laurie Voss <github@seldo.com>
2024-09-01 21:29:57 -0700

    Intermediate representation allowing both react and native powerpoint output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@
 
 import { useState, useCallback, useRef } from "react";
 import crypto from 'crypto';
-
 import {
   Presentation, Slide, Text,
   Shape, Image, render as renderPptx
 } from "react-pptx"
 import Preview from "react-pptx/preview";
+import pptxgen from "pptxgenjs";
 
 export default function Home() {
   const [rawText, setRawText] = useState("");
-  const [ppt, setPpt] = useState<JSX.Element | null>(null);
+  const [intermediate, setIntermediate] = useState();
+  const [presentationPreviews, setPresentationPreviews] = useState<JSX.Element[] | null>(null);
   const [formatCache] = useState(new Map());
   const dialogRef = useRef<HTMLDialogElement>(null);
 
@@ -19,14 +20,43 @@ export default function Home() {
     return crypto.createHash('md5').update(content).digest('hex');
   };
 
-  const downloadPptx = async () => {
-    if (!ppt) {
+  const convertToPptx = async (intermediate) => {
+    let pres = new pptxgen();
+    for (let child of intermediate.children) {
+      let slide = pres.addSlide()
+      let content = child.children
+      for (let el of content) {
+        switch(el.type) {
+          case "text.bullet":
+            slide.addText(
+              el.children[0].content, 
+              {
+                ...el.style,
+                bullet: true
+              }
+            )
+            break;
+          case "text":
+            slide.addText(
+              el.children[0].content,
+              el.style
+            )  
+        }
+      }
+    }
+    return pres
+  }
+
+  const generatePptx = async () => {
+    if (!intermediate) {
       dialogRef.current?.showModal();
       return;
     }
-    let pptx = await renderPptx(ppt,{ outputType: "blob" })
+    let pres = await convertToPptx(intermediate)
+    let presBlob = await pres.write("blob")
+
     const a = document.createElement("a");
-    const url = URL.createObjectURL(pptx as Blob | MediaSource);
+    const url = URL.createObjectURL(presBlob as Blob | MediaSource);
     a.href = url;
     a.download = "presentation.pptx";
     a.click();
@@ -57,68 +87,162 @@ export default function Home() {
     return formattedContent;
   }, [formatCache]);
 
-  const handleConvert = async () => {
-    setPpt(<Presentation>
-      <Slide>
-        <Text style={{
-            x: "10%", y: "20%", w: "80%", h: "40%",
-            fontSize: 80,
-        }}>Generating...</Text>
-      </Slide>
-    </Presentation>); // waiting state
-    
-    let slides = rawText.split("\n\n");
+  const generateIntermediateRepresentation = async () => {
 
-    let pptSlides = await Promise.all(slides.map(async (slide, index) => {
+    let rawTextSlides = rawText.split("\n\n");
+
+    let slides = await Promise.all(rawTextSlides.map(async (slideText, index) => {
       /* the whole slide is 10 inches wide by 5.6 inches high */
-      let firstline = slide.split("\n")[0];
+      let firstline = slideText.split("\n")[0];
 
       // first slide is a title slide
       if (index === 0) {
-        return <Slide>
-          <Text style={{
-            x: 1, y: 2, w: 8, h: 1,
-            fontSize: 80,
-          }}>{firstline}</Text>
-        </Slide>
+        return {
+          type: "slide",
+          children: [
+            {
+              type: "text",
+              style: {
+                x: 1,
+                y: 2,
+                w: 8,
+                h: 1,
+                fontSize: 80
+              },
+              children: [
+                {
+                  type: "string",
+                  content: firstline
+                }
+              ]
+            }
+          ]
+        }
       }
 
       // all other slides follow the same format
       if (firstline.startsWith("- ")) {
         firstline = firstline.slice(2);
       }
-      let formattedContent = await formatWithCache(slide);
+      let formattedContent = await formatWithCache(slideText);
       console.log("Formatted content: ", formattedContent)
 
+      let slide = {
+        type: "slide",
+        children: [] as Record<string, any>[],
+        speakerNotes: slideText
+      }
+
       // first line is big
-      let pptContent = [
-        <Text style={{
-          x: 1, y: 0.7, w: 8, h: 1,
-          fontSize: 40,
-        }}>{firstline}</Text>
-      ]
+      slide.children.push({
+        type: "text",
+        style: {
+          x: 1,
+          y: 0.7,
+          w: 8,
+          h: 1,
+          fontSize: 40
+        },
+        children: [
+          {
+            type: "string",
+            content: firstline
+          }
+        ]
+      })
 
       // subsequent lines are mostly bullet points
-      pptContent.push(formattedContent.split("\n").map((line: string, index: number) => {
+      slide.children = slide.children.concat(formattedContent.split("\n").map((line: string, index: number) => {
         console.log("Line: ", line)
         if (line.startsWith("- ")) {
-          return <Text style={{
-            x: 1, y: 1.4 + (0.7*index), w: 8, h: 1,
-            fontSize: 20,
-          }}><Text.Bullet>{line.slice(2)}</Text.Bullet></Text>
+          return {
+            type: "text.bullet",
+            style: {
+              x: 1,
+              y: 1.4 + (0.7 * index),
+              w: 8,
+              h: 1,
+              fontSize: 20
+            },
+            children: [
+              {
+                type: "string",
+                content: line.slice(2)
+              }
+            ]
+          }
         } else {
-          return <Text style={{
-            x: 1, y: 1.4 + (0.7*index), w: 8, h: 1,
-            fontSize: 20,
-          }}>{line}</Text>  
+          return {
+            type: "text",
+            style: {
+              x: 1,
+              y: 1.4 + (0.7 * index),
+              w: 8,
+              h: 1,
+              fontSize: 20
+            },
+            children: [
+              {
+                type: "string",
+                content: line.slice(2)
+              }
+            ]
+          }
         }
       }))
-      let pptSlide = <Slide>{pptContent}</Slide>
-      return pptSlide
+      return slide
     }));
 
-    let presentation = <Presentation>{pptSlides}</Presentation>
-    setPpt(presentation)
+    let presentation = {
+      type: "presentation",
+      children: slides
+    }
+
+    return presentation
+  }
+
+  const convertPreviewChildren = (children) => {
+    return children.map((child) => {
+      switch (child.type) {
+        // in the previews, each slide is in its own presentation
+        case "slide":
+          return <Presentation><Slide>{convertPreviewChildren(child.children)}</Slide></Presentation>
+        case "text":
+          return <Text style={child.style}>{convertPreviewChildren(child.children)}</Text>
+        case "text.bullet":
+          return <Text style={child.style}><Text.Bullet>{convertPreviewChildren(child.children)}</Text.Bullet></Text>
+        case "string":
+          return child.content
+      }
+    })
+  }
+
+  const convertToPreviews = async (tree) => {
+    return convertPreviewChildren(tree.children)
+  }
+
+  const generatePreviews = async () => {
+    let waitPresentations: JSX.Element[] = [
+      <Presentation key="waiting">
+        <Slide>
+          <Text style={{
+            x: "10%", y: "20%", w: "80%", h: "40%",
+            fontSize: 80,
+          }}>Generating...</Text>
+        </Slide>
+      </Presentation>
+    ];
+    setPresentationPreviews(waitPresentations); // waiting state
+
+    // get intermediate state
+    let intermediate = await generateIntermediateRepresentation()
+    setIntermediate(intermediate)
+    console.log("Intermediate form ", intermediate)
+    // convert it into an array of single-slide presentations plus notes etc.
+    let presentationPreviews = await convertToPreviews(intermediate)
+    console.log("Presentation previews", presentationPreviews)
+
+    setPresentationPreviews(presentationPreviews)
   };
 
   return (
@@ -132,17 +256,26 @@ export default function Home() {
           ></textarea>
         </div>
         <div id="convertButton">
-          <button onClick={handleConvert}>Slidify ➡️</button>
-          <button onClick={downloadPptx}>Download PPTX</button>
+          <button onClick={generatePreviews}>Slidify ➡️</button>
+          <button onClick={generatePptx}>Download PPTX</button>
         </div>
         <div id="slides">
-          {ppt ? <Preview slideStyle={{
-            border: "1px solid black",
-            marginBottom: "15px",
-            boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.25)"
-          }}>
-            {ppt}
-          </Preview> : <div>No slides to display</div>}
+          {presentationPreviews ? (<div>
+            {presentationPreviews.map((ppt: JSX.Element) => {
+              return <div>
+                <Preview slideStyle={{
+                  border: "1px solid black",
+                  marginBottom: "15px",
+                  boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.25)"
+                }}>
+                  {ppt}
+                </Preview>
+                <div style={{ marginBottom: "10px" }}>
+                  Notes etc.
+                </div>
+              </div>
+            })}
+          </div>) : (<div>No slides to display</div>)}
         </div>
       </div>
       <dialog ref={dialogRef}>
